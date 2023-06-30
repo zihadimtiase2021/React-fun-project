9aadbff2c4a37b57943b52d56851ced7d8703233
zihadimtiase2021 <zdimtiase@gmail.com>
2023-06-30 19:30:07 +0600

    all

---
+++ 01-birthday-reminder/setup/src/List.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line react/prop-types
+export default function List({ people }) {
+  return (
+    <>
+      {people.map(people => {
+        const { id, name, age, image } = people;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years old</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+}
